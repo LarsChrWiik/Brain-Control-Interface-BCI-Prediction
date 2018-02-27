dec667efe20119b8cd0eb394210b53539ba4b805
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-02-27 16:40:45 +0000

    main.

--- GUI/main.py
+++ GUI/main.py
@@ -1,7 +1,23 @@
 
-import Importer
+from Importer import Importer
+from Wrapper import Wrapper
+from Visualization import Visualization
 
-filename = "fkjkfjd.mat"
+filename_trian_A = "Subject_A_Train.mat"
+filename_test_A = "Subject_A_Test.mat"
 
-# Import mat file.
-data_raw = Importer.mat(filename)
+wrapper = Wrapper()
+
+def train():
+    # Import mat file.
+    data_raw = Importer.mat(filename_trian_A)
+
+    Visualization.visualize_1(
+        data_raw["Signal"][4],
+        data_raw["Flashing"][0],
+    )
+
+    #wrapper.train(data_raw)
+
+train()
+
