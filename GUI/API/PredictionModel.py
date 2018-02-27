903197f26fe7d1238dc82a4bee627679b2bf8bcf
Veryard L H F <lhfver@cseelab700.essex.ac.uk>
2018-02-27 15:39:36 +0000

    Started working on the Prediction Model { added data structures }

--- GUI/API/PredictionModel.py
+++ GUI/API/PredictionModel.py
@@ -1,13 +1,28 @@
-
+import sklearn
 
 # Prediction model.
 class PredictionModel:
 
+    # Testing Data Structures
+    test_stimulus_code = []
+    test_signal = []
+    test_flashing = []
+
+    # Training Data Structures
+    training_stimulas_code = []
+    training_stimulas_type = []
+    training_target = []
+    training_signal = []
+    training_flashing = []
+
+    # Training Method
     def train(self):
         print("Not implemented")
 
+    # Validation Method
     def validation(self):
         print("Not implemented")
 
+    # Prediction Method
     def predict(self):
         print("Not implemented")
