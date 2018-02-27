fab94d590c6db14174352a60f154288f50b6bbe8
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-02-27 16:34:28 +0000

    Added a file and one rename.

--- GUI/API/Filter.py
+++ GUI/API/Filter.py
@@ -1,18 +1,14 @@
 
 
 
-class Preprocesser:
-
-    # Main preprocessing function.
-    def preprocess(data, statisticObh = None):
-        print("Not implemented")
+class Filter:
 
     # Band filter that removes the spectrum of the signal.
-    def band_filter():
+    def band_filter(self):
         print("Not implemented")
 
     # Enhance the difference between two channels.
-    def laplacian_filer():
+    def laplacian_filer(self):
         print("Not implemented")
 
     # Creates the end result.
