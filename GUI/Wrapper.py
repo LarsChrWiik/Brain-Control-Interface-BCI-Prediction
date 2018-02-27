cb76cebe14732166f74ea66181f9650c5036ee9a
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-02-27 16:39:50 +0000

    minor change.

--- GUI/Wrapper.py
+++ GUI/Wrapper.py
@@ -1,17 +1,17 @@
 
-from API import BciObject
+from API.BciObject import BciObject
 
 class Wrapper:
 
     bciObject = BciObject()
 
-    def train(self):
-        return self.bciObject.train()
+    def train(self, data_raw):
+        return self.bciObject.train(data_raw)
 
-
+    """
     def preprocess(self):
         return self.bciObject.preprocess()
-
+    """
 
     def predict(self):
         return self.bciObject.predict()
