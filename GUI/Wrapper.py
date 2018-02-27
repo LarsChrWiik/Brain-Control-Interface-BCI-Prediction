1d3cb483d9c52f051d4d1cbf4262898a82d98391
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-02-27 19:59:55 +0000

    Removed preprocessor def.

--- GUI/Wrapper.py
+++ GUI/Wrapper.py
@@ -1,17 +1,15 @@
 
 from API.BciObject import BciObject
 
+# Wrapper class for the GUI.
+# This class is used as the bridge between the GUI and the API.
 class Wrapper:
 
     bciObject = BciObject()
 
+    # Train a prediction model using raw data.
     def train(self, data_raw):
         return self.bciObject.train(data_raw)
-
-    """
-    def preprocess(self):
-        return self.bciObject.preprocess()
-    """
 
     def predict(self):
         return self.bciObject.predict()
