52d333a581e81005cbfee797941a97cf8c83e3ed
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-02-27 16:43:38 +0000

    minor change.

--- GUI/API/BciObject.py
+++ GUI/API/BciObject.py
@@ -1,13 +1,18 @@
 
+from API.Filter import Filter
+from API.PredictionModel import PredictionModel
+
 class BciObject:
 
-    preprocesser = Preprocesser()
     prediction_model = PredictionModel()
 
     def get_statistics(self):
         print("Not implemented")
 
-    def train():
+    def train(self, data_raw):
+        data_filtered = Filter.filter(data_raw)
+
+        # Send to model.
         print("Not implemented")
 
     def predict():
