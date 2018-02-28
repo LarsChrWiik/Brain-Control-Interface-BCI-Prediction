6de445d7af9937c2131f7d2a64858a690457a1c8
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-02-28 15:23:31 +0000

    Minor chang.

--- GUI/API/BciObject.py
+++ GUI/API/BciObject.py
@@ -15,14 +15,23 @@ class BciObject:
         # Pre-process the data.
         X, Y = self.preprocessor.preprocess(data_raw)
 
-        # Validate performane.
+        # Validate performance.
         self.prediction_model.cross_validation(X, Y)
 
+    """
+    Train the prediction model. 
+    """
     def train(self, data_raw):
         print("Not implemented")
 
+    """
+    Make a prediction using the prediction model. 
+    """
     def predict(self):
         print("Not implemented")
 
+    """
+    Extract statistics from the model building process. 
+    """
     def get_statistics(self):
         print("Not implemented")
