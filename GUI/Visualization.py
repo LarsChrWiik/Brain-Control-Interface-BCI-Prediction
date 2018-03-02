cb9c47d8844fa9ec316717c3a29183d4ac8aaa99
paul <pas43@aber.ac.uk>
2018-03-02 00:55:45 +0000

    Filter - Changed data types. File should work with dictionary: AUSSUMING! the data input has the key 'Signal' Preprocessor - Uncomented the Filter call to check with expected input. Visualization - Put the old pyplot code from Filter here, not working yet. jsut a quick copy & paste job.

--- GUI/Visualization.py
+++ GUI/Visualization.py
@@ -1,5 +1,6 @@
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 # TODO: This file should be removed, its only for development.
 """
@@ -22,4 +23,18 @@ class Visualization:
         # Visualize
         graph = plt.plot(stimulus_type)
         plt.show()
+
+    #todo - check the function inputs as definitions needs data its not getting
+    #     #@staticmethod
+    # def channelGraph(channelData):
+    #     ##Graph - This belongs somewhere else probably.
+    #     t = np.linspace(0, len(channelData), len(channelData), endpoint=False)
+    #     plt.plot(t, y)
+    #     plt.grid(True)
+    #     plt.axis('tight')
+    #     tick = np.linspace(0, len(data), len(data) / samplingFreq)
+    #     plt.xticks(tick, range(lengthOfTestSeconds)) ##32 seconds per test?
+    #     plt.xlabel("Time in Seconds")
+    #     plt.legend(loc='upper left')
+    #     plt.show()
 
