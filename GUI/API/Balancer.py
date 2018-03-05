016c2e5c6720dcbc1ac64f353d0d0b855bd543b6
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-03-05 15:50:45 +0000

    Added a another balance function.

--- GUI/API/Balancer.py
+++ GUI/API/Balancer.py
@@ -23,3 +23,34 @@ class Balancer:
                 y_counter += 1
 
         return X_new, Y_new
+
+    @staticmethod
+    def balance_equal_2(X, Y):
+        y_0 = 0
+        y_1 = 0
+        for example in Y:
+            y_0 += len([x for x in example if x == 0])
+            y_1 += len([x for x in example if x == 1])
+
+        min_size = min(y_0, y_1)
+
+        X_new = []
+        x_counter = 0
+        Y_new = []
+        y_counter = 0
+        for i in range(len(Y)):
+            X_example = []
+            Y_example = []
+            for j in range(len(Y[0])):
+                if Y[i][j] == 0 and x_counter < min_size:
+                    X_example.append(X[i][j])
+                    Y_example.append(Y[i][j])
+                    x_counter += 1
+                elif Y[i][j] == 1 and y_counter < min_size:
+                    X_example.append(X[i][j])
+                    Y_example.append(Y[i][j])
+                    y_counter += 1
+            X_new.append(X_example)
+            Y_new.append(Y_example)
+
+        return X_new, Y_new
