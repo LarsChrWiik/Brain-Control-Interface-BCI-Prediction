dd05cbc640a243200b15adbeff068cf8e944e13c
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-03-05 15:50:14 +0000

    Fixed the pipeline.

--- GUI/API/Preprocessor.py
+++ GUI/API/Preprocessor.py
@@ -3,6 +3,7 @@ from API.Filter import Filter
 from API.Chunker import Chucker
 from API.Formater import Formater
 from API.Balancer import Balancer
+from API.PLV import PLV
 
 class Preprocessor:
 
@@ -11,22 +12,27 @@ class Preprocessor:
 
         # TODO: Needs to be re-implemented. (DICT not np.array)
         # Filter the input data.
-        #data_filtered = Filter.filter(data_raw)
-        #print(type(data_filtered))
+        data_filtered = Filter.filter(data_raw)
+        # print(type(data_filtered))
 
         # TODO: Use data_filtered when filter is working.
         # Chunk the filtered data.
-        data_chunked, targets = Chucker.chunk_train(data_raw)
-
-        # Format the chunked data.
-        X, Y = Formater.format_chunked_data(data_chunked, targets)
+        X, Y = Chucker.chunk_train(data_raw)
 
         # Shrink the data size.
-        X = Preprocessor.shrink_data(X, shrink_percent)
-        Y = Preprocessor.shrink_data(Y, shrink_percent)
+        X = Preprocessor.shrink_data_2(X, shrink_percent)
+        Y = Preprocessor.shrink_data_2(Y, shrink_percent)
 
         # Balance the data.
-        X, Y = Balancer.balance_equal(X, Y)
+        X, Y = Balancer.balance_equal_2(X, Y)
+
+        # TODO: Add metrics.
+        print("before")
+        X = PLV.apply(X)
+        print("after")
+
+        # Format the chunked data.
+        X, Y = Formater.format(X, Y)
 
         # Returns
         if not with_targets:
@@ -39,6 +45,13 @@ class Preprocessor:
     @staticmethod
     def shrink_data(data, percent):
         return data[:int(len(data) - percent * len(data))]
+
+    """
+    Shrink data. 
+    """
+    @staticmethod
+    def shrink_data_2(data, percent):
+        return [x[:int(len(x) - percent * len(x))] for x in data]
 
 
 
