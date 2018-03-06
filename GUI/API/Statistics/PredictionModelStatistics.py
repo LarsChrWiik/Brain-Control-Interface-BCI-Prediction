04fb3f2e8b0683163b0090e0e5042308a444e13f
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-03-06 16:44:23 +0000

    Minor change.

--- GUI/API/Statistics/PredictionModelStatistics.py
+++ GUI/API/Statistics/PredictionModelStatistics.py
@@ -1,5 +1,12 @@
 
 class PredictionModelStatistics:
 
+    # TODO: Add what is needed.
+    accuracy = None
+    precision = None
+    recall = None
+    f1 = None
+    support = None
+
     def clear_statistics(self):
         pass
