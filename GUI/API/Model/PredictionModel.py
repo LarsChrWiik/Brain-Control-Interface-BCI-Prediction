4dfc9ab65cd1fc916136c94e40fcc09da545557e
Lewis veryard <lewis.veryard@hoitmail.co.uk>
2018-03-08 17:06:58 +0000

    added comments and make code tidyer

--- GUI/API/Model/PredictionModel.py
+++ GUI/API/Model/PredictionModel.py
@@ -10,9 +10,7 @@ class PredictionModel:
         self.model_statistics = PredictionModelStatistics()
 
     def train(self, classifiers, X, Y, test_size=0.3):
-
         predictions = []
-
         # Divide into training and test set.
         training_input, testing_input, training_target, testing_target = train_test_split(
             X,
@@ -20,22 +18,20 @@ class PredictionModel:
             test_size=test_size
         )
         i = 0
+        # Train
         while(i < len(classifiers)):
             classifiers[i].fit(training_input, training_target)
             i+=1
 
         i = 0
-
+        # Test
         while(i < len(classifiers)):
             predictions.append(classifiers[i].predict(testing_input))
             i+=1
-
+        # Combine
         prediction_final = self.combine(predictions)
-
+        # Add statistics
         self.model_statistics.fill(testing_target, prediction_final)
-
-
-
 
     def predict(self, classifiers, X):
         predictions = []
@@ -43,6 +39,8 @@ class PredictionModel:
         while(i < len(classifiers)):
            predictions.append(classifiers[i].predict(X))
            i+=1
+        return self.combine(predictions)
+
 
     def combine(self, predictions):
         ans = []
