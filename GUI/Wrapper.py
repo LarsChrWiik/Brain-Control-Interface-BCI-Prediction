fb10d01967d2d694be71b95fac29b02ceb501f4c
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-03-09 18:17:17 +0000

    Added print statistics in wrapper.

--- GUI/Wrapper.py
+++ GUI/Wrapper.py
@@ -17,13 +17,18 @@ class Wrapper:
         )
 
     def train(self, data_raw, with_targets=True, shrink_percent=0, should_balance=True, verbose=False):
-        return self.bciObject.train(
+        self.bciObject.train(
             data_raw=data_raw,
             with_targets=with_targets,
             shrink_percent=shrink_percent,
             should_balance=should_balance,
             verbose=verbose
         )
+
+        statistics = self.bciObject.preprocessor.preprocess_statistics.raw_signal
+
+        print(statistics)
+
 
     def predict(self, data_raw):
         return self.bciObject.predict(data_raw)
