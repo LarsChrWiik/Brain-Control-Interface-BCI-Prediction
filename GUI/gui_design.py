1ac6e9cd4cfaa7917d1370fa965bbc5b3c7f6a8a
mrmad <vivraaj@gmail.com>
2018-03-09 01:07:37 +0000

    cleaned_up_gui

--- GUI/gui_design.py
+++ GUI/gui_design.py
@@ -1,33 +1,41 @@
 import sys
-from PyQt4 import QtGui, QtCore
+
+from PyQt5.QtCore import QCoreApplication, Qt
+from PyQt5.QtGui import QIcon, QColor, QFont 
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton, QAction, QMessageBox
+from PyQt5.QtWidgets import QCalendarWidget, QColorDialog, QTextEdit, QFileDialog
+from PyQt5.QtWidgets import QCheckBox, QProgressBar, QComboBox, QLabel, QStyleFactory, QLineEdit, QInputDialog
+
+
+
 from Wrapper import Wrapper
 from Importer import Importer
 
 wrapper = Wrapper()
 
 # main window inherit from main window
-class Window(QtGui.QMainWindow):
+class Window(QMainWindow):
 
     def __init__(self):
         # use super to return parent object
         super(Window, self).__init__()
         self.setGeometry(50, 50, 650, 450)
         self.setWindowTitle("Brain Scanner!")
-        #self.setWindowIcon(QtGui.QIcon('pythonlogo.png'))
+        #self.setWindowIcon(QIcon('pythonlogo.png'))
         
         # below for file menu
-        extractAction = QtGui.QAction("&GET TO THE CHOPPAH!!!", self)
+        extractAction = QAction("&GET TO THE CHOPPAH!!!", self)
         extractAction.setShortcut("Ctrl+Q")
         # below for status bar
         extractAction.setStatusTip('Leave The App')
         extractAction.triggered.connect(self.close_application)
 
-        openFile = QtGui.QAction("&Open File", self)
+        openFile = QAction("&Open File", self)
         openFile.setShortcut("Ctrl+O")
         openFile.setStatusTip('Open File')
         openFile.triggered.connect(self.file_open)
         
-        saveFile = QtGui.QAction("&Save File", self)
+        saveFile = QAction("&Save File", self)
         saveFile.setShortcut("Ctrl+S")
         saveFile.setStatusTip('Save File')
         saveFile.triggered.connect(self.file_save)
@@ -45,28 +53,28 @@ class Window(QtGui.QMainWindow):
         self.home()
 
     def home(self):
-        btn1 = QtGui.QPushButton("Train", self)
+        btn1 = QPushButton("Train", self)
         btn1.clicked.connect(self.file_open)
         btn1.resize(191,41)
         btn1.move(420,100)
 
-        btn2 = QtGui.QPushButton("Predict", self)
-        btn2.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        btn2 = QPushButton("Predict", self)
+        btn2.clicked.connect(self.close_application)
         btn2.resize(191,41)
         btn2.move(420,150)
 
-        btn3 = QtGui.QPushButton("Load", self)
-        btn3.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        btn3 = QPushButton("Load", self)
+        btn3.clicked.connect(self.close_application)
         btn3.resize(191,41)
         btn3.move(420,200)
 
-        btn4 = QtGui.QPushButton("Save", self)
-        btn4.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        btn4 = QPushButton("Save", self)
+        btn4.clicked.connect(self.close_application)
         btn4.resize(191,41)
         btn4.move(420,250)
 
-        btn5 = QtGui.QPushButton("Get Statistics", self)
-        btn5.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        btn5 = QPushButton("Get Statistics", self)
+        btn5.clicked.connect(self.close_application)
         btn5.resize(191,41)
         btn5.move(420,300)
 
@@ -78,28 +86,28 @@ class Window(QtGui.QMainWindow):
         #self.btn.move(200,120)
         #self.btn.clicked.connect(self.download)
 
-        label_1 = QtGui.QLabel("Product \n  Name", self)
-        label_1.setFont(QtGui.QFont("Times", 40, QtGui.QFont.Bold))
+        label_1 = QLabel("Product \n  Name", self)
+        label_1.setFont(QFont("Times", 40, QFont.Bold))
         label_1.resize(321, 201)
         label_1.move(30,30)
 
-        label_2 = QtGui.QLabel("Insert Product Logo Here", self)
+        label_2 = QLabel("Insert Product Logo Here", self)
         label_2.resize(200, 16)
         label_2.move(20, 310)
 
-        label_3 = QtGui.QLabel("Version ......", self)
+        label_3 = QLabel("Version ......", self)
         label_3.resize(171, 16)
         label_3.move(20, 330)
 
-        label_4 = QtGui.QLabel("(c) 2018 - ....", self)
+        label_4 = QLabel("(c) 2018 - ....", self)
         label_4.resize(171, 16)
         label_4.move(20, 350)
 
-        label_5 = QtGui.QLabel("University of Essex", self)
+        label_5 = QLabel("University of Essex", self)
         label_5.resize(171, 16)
         label_5.move(20, 370)
 
-        label_6 = QtGui.QLabel("Essex, United Kingdom", self)
+        label_6 = QLabel("Essex, United Kingdom", self)
         label_6.resize(171, 16)
         label_6.move(20, 390)
 
@@ -108,16 +116,16 @@ class Window(QtGui.QMainWindow):
         self.show()
 
     def file_open(self):
-        name = QtGui.QFileDialog.getOpenFileName(self, 'Open File')
+        name = QFileDialog.getOpenFileName(self, 'Open File')
         #print(name)
         #file = open(name,'r')
 
         #self.editor()
 
-        choice = QtGui.QMessageBox.question(self, 'Extract!',
+        choice = QMessageBox.question(self, 'Extract!',
                                             "Are you sure you want to train?",
-                                            QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
-        if choice == QtGui.QMessageBox.Yes:
+                                            QMessageBox.Yes | QMessageBox.No)
+        if choice == QMessageBox.Yes:
             print(name)
             lol = Importer.mat(name)
             print(lol)
@@ -139,7 +147,7 @@ class Window(QtGui.QMainWindow):
         #    self.textEdit.setText(text)
 
     def file_save(self):
-        name = QtGui.QFileDialog.getSaveFileName(self, 'Save File')
+        name = QFileDialog.getSaveFileName(self, 'Save File')
         file = open(name,'w')
         text = self.textEdit.toPlainText()
         file.write(text)
@@ -157,21 +165,21 @@ class Window(QtGui.QMainWindow):
             self.progress.setValue(self.completed)
 
     def color_picker(self):
-        color = QtGui.QColorDialog.getColor()
+        color = QColorDialog.getColor()
         self.styleChoice.setStyleSheet("QWidget { background-color: %s}" % color.name())
 
     def close_application(self):
-        choice = QtGui.QMessageBox.question(self, 'Extract!',
+        choice = QMessageBox.question(self, 'Extract!',
                                             "Are you sure you want to exit?",
-                                            QtGui.QMessageBox.Yes | QtGui.QMessageBox.No)
-        if choice == QtGui.QMessageBox.Yes:
+                                            QMessageBox.Yes | QMessageBox.No)
+        if choice == QMessageBox.Yes:
             print("Extracting Naaaaaaoooww!!!!")
             sys.exit()
         else:
             pass
 
 def run():
-    app = QtGui.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     GUI = Window()
     sys.exit(app.exec_())
 
