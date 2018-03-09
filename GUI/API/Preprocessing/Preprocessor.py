c4f3ef4545beb6fb10b311105149097d952d4c17
Lars Wiik <Larswiik@Lars-MacBook-Pro.local>
2018-03-09 17:04:12 +0000

    Added verbose.

--- GUI/API/Preprocessing/Preprocessor.py
+++ GUI/API/Preprocessing/Preprocessor.py
@@ -27,21 +27,34 @@ class Preprocessor:
             data_raw,
             with_targets=True,
             shrink_percent=0,
-            should_balance=True
+            should_balance=True,
+            verbose=True
     ):
+        if verbose: print("Started pre-processing")
+
+        if verbose: print("Started Filtering")
+
         # Filter the input data.
         data_filtered = Filter.filter(data_raw)
 
+        if verbose: print("Started Chunking")
+
         # Chunk the filtered data.
         X_chunked, Y_chunked = Chucker.chunk_train(data_filtered)
+
+        if verbose: print("Started Shrinking")
 
         # Shrink the data by a given percent.
         X = Shrinker.shrink_data_with_examples(X_chunked, shrink_percent)
         Y = Shrinker.shrink_data_with_examples(Y_chunked, shrink_percent)
 
+        if verbose: print("Started Balancing")
+
         # Balance the data according to targets (0 or 1).
         if with_targets and should_balance:
             X, Y = Balancer.balance_equal_with_examples(X, Y)
+
+        if verbose: print("Started Metrics")
 
         # Applying metric (Phase)
         #X = Phase.apply(X)
@@ -52,8 +65,12 @@ class Preprocessor:
         # Applying metric (PLI)
         X = PLI.apply(X)
 
+        if verbose: print("Started Formating")
+
         # Format the data into a usable format for classifiers.
         X, Y = Formater.format(X, Y)
+
+        if verbose: print("Started MinMaxScaler")
 
         # Normalize the data. (Between 0 and 1)
         scaler = MinMaxScaler()
